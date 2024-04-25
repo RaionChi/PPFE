80c55ff77d91a28e60a8ff8dc13ff4f640fd77b7
ALion <alexis.lion@brassart.fr>
2024-04-25 23:50:42 +0200

    1er push

---
+++ Assets/script/Car_controller.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Car_controller : MonoBehaviour
+{
+    public Text TxtSpeed;
+    public WheelCollider front_left;
+    public WheelCollider front_right;
+    public WheelCollider back_left;
+    public WheelCollider back_right;
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
