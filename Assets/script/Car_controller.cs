a14a1f42995aaebb472e38861950164aa6ca142c
ALion <alexis.lion@brassart.fr>
2024-05-07 22:22:09 +0200

    Mise en place vitesse et  direction

--- Assets/script/Car_controller.cs
+++ Assets/script/Car_controller.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Car_controller : MonoBehaviour
 {
-    public Text TxtSpeed;
+    public TMP_Text textSpeed;
     public WheelCollider front_left;
     public WheelCollider front_right;
     public WheelCollider back_left;
@@ -15,6 +16,7 @@ public class Car_controller : MonoBehaviour
     public float MaxSpeed = 200f;
     public int Brake = 10000;
     public float CoefAcceleration = 10f;
+    public float WhellAngleMAx = 10f;
 
 
     void Start()
@@ -25,9 +27,32 @@ public class Car_controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.Z))
-        {
+        Speed = GetComponent<Rigidbody>().velocity.magnitude * 3.6f;
+        textSpeed.text = "Speed : " + (int) Speed ;
 
+        //Accelleration
+        if (Speed < MaxSpeed)
+        {
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                back_left.brakeTorque = 0;
+                back_right.brakeTorque = 0;
+                back_left.motorTorque = Input.GetAxis("Vertical") * Torque * CoefAcceleration * Time.deltaTime;
+                back_right.motorTorque = Input.GetAxis("Vertical") * Torque * CoefAcceleration * Time.deltaTime;
+            }
         }
+        
+        //Déceleration
+        if (!Input.GetKey(KeyCode.UpArrow) )
+        {
+            back_left.motorTorque = 0;
+            back_right.motorTorque = 0;
+            back_left.brakeTorque = Brake * CoefAcceleration * Time.deltaTime;
+            back_right.brakeTorque = Brake * CoefAcceleration * Time.deltaTime;
+        }
+
+        //direction du vehicule
+        front_left.steerAngle = Input.GetAxis("Horizontal") * WhellAngleMAx;
+        front_right.steerAngle = Input.GetAxis("Horizontal") * WhellAngleMAx;
     }
 }
